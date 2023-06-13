293b7fd0effb371021c6048f88136aa59f1a4a51
Nishant Sachdeva <107066337+hardware-sensei@users.noreply.github.com>
2023-06-13 13:47:02 +0530

    Update true_dpram_dclk_tb.v

--- true_dpram_dclk_tb.v
+++ true_dpram_dclk_tb.v
@@ -6,17 +6,7 @@ module true_dpram_dclk_tb();
 	reg [ADDRESS - 1:0] addr_A, addr_B;
 	wire [WIDTH - 1:0] data_out_A, data_out_B;
 
-	true_dpram_dclk dut(
-		.data_in_A(data_in_A),
-		.data_in_B(data_in_B),
-		.addr_A(addr_A),
-		.addr_B(addr_B),
-		.wr_en_A(wr_en_A),
-		.wr_en_B(wr_en_B),
-		.clk_A(clk_A),
-		.clk_B(clk_B),
-		.data_out_A(data_out_A),
-		.data_out_B(data_out_B) );
+	true_dpram_dclk dut(.*);
 
 	initial begin
 		clk_A = 1'b1;
@@ -31,8 +21,10 @@ module true_dpram_dclk_tb();
 	initial begin
 		$monitor("TIME = %0t,\nAddress @ A = %0h , Data @ A = %0h,\nAddress @ B = %0h, Data @ B = %0h", $time, addr_A, data_in_A, addr_B, data_in_B);
 		$dumpfile("true_dpram_dclk_tb.vcd");
-		$dumpvars(0, true_dpram_dclk_tb);
-		
+		$dumpvars;
+	end
+	
+	initial begin
 		wr_en_A = 1'b0; wr_en_B = 1'b0;
 		#100;
 		
